e77921133a74d180e0ba099cc47c848276cf1645
nonsosky <nonsoskyokpara@gmail.com>
2019-07-31 18:06:05 +0100

    first commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 let mongoURI = "";
 
 if(process.env.NODE_ENV === "production"){
-  mongoURI = "mongodb://<dbuser>:<dbpassword>@ds157641.mlab.com:57641/result-management-system-prod";
+  mongoURI = "mongodb://nonsosky:nonsosky1@ds157641.mlab.com:57641/result-management-system-prod";
 } else {
   mongoURI = "mongodb://127.0.0.1:27017/resultManagement";
 }
